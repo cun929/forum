9c3654651ecc1b0c7e37936422622f4ed1a69a76
iRoot <1037376595@qq.com>
2021-08-24 19:14:18 +0800

    no message

--- src/views/User/uploadArticle/index.vue
+++ src/views/User/uploadArticle/index.vue
@@ -1,9 +1,53 @@
 <template>
-    <input type="text" v-model="ip" />
-    <div v-html="ip"></div>
+    <div id="uploadArticle">
+        <div>123123</div>
+        <div>
+            <div>
+                <textarea type="text" v-model="s" />
+            </div>
+            <div v-html="c"></div>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import marked from 'marked'
+import { ref, computed } from 'vue'
+let s = ref<String>('# 12321')
+let c = computed(() => {
+    return marked(s.value, { sanitize: true })
+})
 
-let ip = ref<String>('sdas')
+
 </script>
+<style lang="less">
+#uploadArticle {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    > div {
+        &:nth-child(1) {
+            height: 70px;
+            background-color: #fff;
+            width: 100%;
+        }
+        &:nth-child(2) {
+            height: calc(~"100vh - 70px");
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+            textarea {
+                background-color: #fff;
+                border: none;
+                padding: 10px;
+                width: 100%;
+                height: 100%;
+                outline: none;
+            }
+            > div {
+                padding: 20px;
+                flex: 1;
+            }
+        }
+    }
+}
+</style>
